4dc7c264614abe280937f842e6d3bd792aa698f6
Ivan <ipkozyrin@gmail.com>
2023-10-11 16:58:05 -0300

    WIP

--- src/routes/api/1/+server.ts
+++ src/routes/api/1/+server.ts
@@ -1,17 +1,22 @@
 import { error } from '@sveltejs/kit';
 
-/** @type {import('./$types').RequestHandler} */
+
+export const prerender = true
+
+
 export function GET({ url }) {
-  const min = Number(url.searchParams.get('min') ?? '0');
-  const max = Number(url.searchParams.get('max') ?? '1');
+  // const min = Number(url.searchParams.get('min') ?? '0');
+  // const max = Number(url.searchParams.get('max') ?? '1');
+  //
+  // const d = max - min;
+  //
+  // if (isNaN(d) || d < 0) {
+  //   throw error(400, 'min and max must be numbers, and min must be less than max');
+  // }
+  //
+  // const random = min + Math.random() * d;
+  //
+  // return new Response(String(random));
 
-  const d = max - min;
-
-  if (isNaN(d) || d < 0) {
-    throw error(400, 'min and max must be numbers, and min must be less than max');
-  }
-
-  const random = min + Math.random() * d;
-
-  return new Response(String(random));
+  return new Response('aaa');
 }
