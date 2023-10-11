7aa28c005f297c620f64c96a9ac2f34db2978ca3
Ivan <ipkozyrin@gmail.com>
2023-10-11 18:29:36 -0300

    WIP

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,11 +2,15 @@ import type { LayoutLoad } from './$types'
 import {DEFAULT_LANG} from '../lib/constants';
 import { addMessages, init } from 'svelte-intl-precompile';
 
-// window.navigator.language
+
 export const prerender = true
 export const ssr = true
+
+// TODO: если включить то не отображается левая панель
 //export const csr = false
 
+// TODO: можно в куки установить
+// window.navigator.language
 
 export const load: LayoutLoad = async (event) => {
   const lang = event.params.lang || DEFAULT_LANG
