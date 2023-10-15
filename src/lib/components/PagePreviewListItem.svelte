eac327d9048fd81b309e6acc23cffda4e373cb9a
Ivan <ipkozyrin@gmail.com>
2023-10-15 14:32:02 -0300

    WIP

--- src/lib/components/PagePreviewListItem.svelte
+++ src/lib/components/PagePreviewListItem.svelte
@@ -13,27 +13,30 @@
 
 <a
   href={`${baseUrl}/${name}`}
-  class="mb-6 flex w-full px-5 py-5 bg-white dark:bg-gray-800 border rounded-lg border-gray-200 dark:border-gray-700"
+  class="mb-6 block px-5 py-5 bg-white dark:bg-gray-800 border rounded-lg border-gray-200 dark:border-gray-700"
 >
-  <div class="w-80">
-    <Img class="w-80" src="/images/thumbs/{name.replace('/', '_')}.jpg" />
-  </div>
-  <div>
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-      {title}
-    </h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">{descr}</p>
-
-    <div class="flex">
-      <div class="flex-1">
-        {#if tags && tags.length}
-          <TagList tags={tags} />
-        {/if}
-      </div>
+  <h5 class="mb-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+    {title}
+  </h5>
+  <div class="md:flex w-full">
+    <div>
+      <Img
+        class="w-80"
+        src="/images/thumbs/{name.replace('/', '_')}.jpg"
+        loading="lazy"
+        alt="{title} thumbnail"
+      />
 
       {#if date}
-        <div class="text-sm flex items-end">{date}</div>
+        <div class="mt-2 text-sm text-gray-500 dark:text-gray-400">{date}</div>
       {/if}
+
+      {#if tags && tags.length}
+        <TagList tags={tags} class="mt-2" />
+      {/if}
+    </div>
+    <div class="md:ml-4 flex-1">
+      <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">{descr}</p>
     </div>
   </div>
 </a>
