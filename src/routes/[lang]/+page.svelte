6c5f9bd1826dd6fc5fef61d5250941b6b05d6d17
Ivan <ipkozyrin@gmail.com>
2023-10-21 21:02:36 -0300

    WIP

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -1,6 +1,15 @@
 <script>
+import {getContext} from "svelte";
 import {t} from '$lib/store/t'
+import {ALL_TAGS_CONTEXT} from '$lib/constants'
 import RenderHtml from "$lib/components/RenderHtml.svelte"
+import PageHeader from '$lib/components/PageHeader.svelte'
+import TagsGrid from '$lib/components/TagsGrid.svelte'
+
+
+let allTags
+
+$: allTags = getContext(ALL_TAGS_CONTEXT)
 
 export let data
 </script>
@@ -13,5 +22,10 @@ export let data
 <section>
 	<div class="html-block">
 		<RenderHtml html={data.result.html} />
+	</div>
+
+	<div class="mt-10">
+		<PageHeader>{$t('link.allTags')}</PageHeader>
+		<TagsGrid tags={$allTags} />
 	</div>
 </section>
