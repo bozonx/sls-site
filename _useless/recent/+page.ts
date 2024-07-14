b4c890a31db5f9f98525a1696047c018f2509a74
Ivan <ipkozyrin@gmail.com>
2024-07-14 20:52:17 -0300

    WIP

--- _useless/recent/+page.ts
+++
@@ -1,8 +0,0 @@
-import {redirect} from '@sveltejs/kit';
-import type {PageLoad} from './$types';
-
-
-// redirect to the first page
-export const load: PageLoad = (event) => {
-  throw redirect(307, event.url.pathname + '/1')
-};
