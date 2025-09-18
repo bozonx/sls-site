6b1ff2c28a29d5e66b04eafc95c3ba3620b36dbc
Ivan <ipkozyrin@gmail.com>
2025-09-18 19:52:37 -0300

    add

--- src/.vitepress/props.js
+++ src/.vitepress/props.js
@@ -2,5 +2,4 @@ export const PROPS = {
   hostname: "https://p-libereco.org",
   repo: "https://github.com/bozonx/sls-site",
   blogUrl: "https://blog.p-libereco.org",
-  footerCopyright: "Copyright © 2024-present La sistemo de persona libereco.",
 };
